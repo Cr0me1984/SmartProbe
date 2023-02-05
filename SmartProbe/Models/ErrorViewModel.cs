602cbb0e2268f9bddcddbd18c7c44b13c409d0d7
Rome Rodriguez <kyle.rodriguez.kr@gmail.com>
2023-02-05 19:13:54 +0800

    Add project files.

---
+++ SmartProbe/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmartProbe.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
